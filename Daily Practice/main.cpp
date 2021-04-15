1065eeb663beab01aa0ac9d16d5baf6892ecb05a
Kevin <vinrazer77@gmail.com>
2021-04-15 12:45:01 +0700

    Initial Commit

---
+++ Daily Practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Daily Practice
+//
+//  Created by Kevin on 15/04/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
