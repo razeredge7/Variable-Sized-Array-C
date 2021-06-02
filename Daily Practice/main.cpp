b3ceb539d36e7f1992335f097d3905c1f9504938
Kevin <vinrazer77@gmail.com>
2021-06-02 12:51:58 +0700

    Variable Sized Arrays

--- Daily Practice/main.cpp
+++ Daily Practice/main.cpp
@@ -1,14 +1,53 @@
-//
-//  main.cpp
-//  Daily Practice
-//
-//  Created by Kevin on 15/04/21.
-//
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main() {
+    int num = 0;
+    int varLengArrInt;
+    vector<int> values;
+    vector<int> baris;
+    
+    vector<vector<int>> varK;
+    vector<vector<int>> queries;
+    // int varK[100][100];
+    int varKa;
+    int varKb;
+    int counter = 0;
+    int counterk = 0;
+    int counterKk = 0;
+    int a = 0;
+    int b = 0;
+    
+
+    
+    //2 input
+    for(counter = 0; counter<2; counter++){
+        cin >> num;
+        values.push_back(num);
+    }
+    
+    //Input how many array here
+    for(counter = 0; counter < values[0]; counter++){
+        
+        cin>>varLengArrInt;
+        baris.push_back(varLengArrInt);
+        vector<int> temp;
+        
+        for(counterk = 0;counterk < baris[counter]; counterk++){
+            cin>>varKa;
+            temp.push_back(varKa);
+        }
+        varK.push_back(temp);
+    }
+        
+    for(counter = 0; counter < values[1]; counter++){
+        cin>>a>>b;
+        cout<<varK[a][b]<<"\n";
+    }
     return 0;
 }
